32dce740131ef3b13ef298d3fe5ef2ad5a8a0752
4nh3k <21520579@gm.uit.edu.com>
2024-11-04 15:13:44 +0700

    Add authorize

--- Ecommerce.Web/Controllers/ProductsController.cs
+++ Ecommerce.Web/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.Domain.Models;
 using Ecommerce.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Web.Controllers
@@ -24,21 +25,21 @@ namespace Ecommerce.Web.Controllers
             var product = await _productService.GetProductByIdAsync(id);
             return product == null ? NotFound() : Ok(product);
         }
-
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> CreateProduct(Product product)
         {
             var createdProduct = await _productService.AddProductAsync(product);
             return CreatedAtAction(nameof(GetProduct), new { id = createdProduct.Id }, createdProduct);
         }
-
+        [Authorize]
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateProduct(int id, Product product)
         {
             var updatedProduct = await _productService.UpdateProductAsync(id, product);
             return updatedProduct == null ? NotFound() : Ok(updatedProduct);
         }
-
+        [Authorize]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
--- Ecommerce.Web/Controllers/UserController.cs
+++ Ecommerce.Web/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.Domain.Models;
 using Ecommerce.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Web.Controllers
@@ -22,21 +23,21 @@ namespace Ecommerce.Web.Controllers
             if (user == null) return NotFound();
             return Ok(user);
         }
-
+        [Authorize]
         [HttpPost]
         public async Task<ActionResult<User>> CreateUser(User user)
         {
             var createdUser = await _userService.CreateUserAsync(user);
             return CreatedAtAction(nameof(GetUser), new { id = createdUser.Id }, createdUser);
         }
-
+        [Authorize]
         [HttpPut]
         public async Task<ActionResult<User>> UpdateUser(User user)
         {
             var updatedUser = await _userService.UpdateUserAsync(user);
             return Ok(updatedUser);
         }
-
+        [Authorize]
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteUser(int id)
         {
